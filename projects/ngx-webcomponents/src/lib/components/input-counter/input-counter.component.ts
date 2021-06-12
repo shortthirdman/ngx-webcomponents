b85a908a05ec54d66cb8455278aa9f1301d6b258
Swetank Mohanty <swetank.mohanty@outlook.com>
2021-06-12 21:21:06 +0530

    [Component] Cleanup of HTML files

--- projects/ngx-webcomponents/src/lib/components/input-counter/input-counter.component.ts
+++ projects/ngx-webcomponents/src/lib/components/input-counter/input-counter.component.ts
@@ -1,39 +1,46 @@
-import { Component, OnInit, Input, Output, EventEmitter, OnChanges, SimpleChanges, ViewChild } from '@angular/core';
+import { Component, OnInit, Input, ViewChild, AfterViewInit } from '@angular/core';
+import { ElementRef, HostListener } from '@angular/core';
 
 @Component({
   selector: 'wui-inputcounter',
-  templateUrl: './input-counter.component.html',
+  template: `
+	  <div class="wui">
+		  <label class="field">
+			  <input id= "wuiInput" class="field__input" [(ngModel)]="textValue" type="text" placeholder="{{ placeholderText }}" maxlength="maxLength"
+			  	(input)="onInputChange($event)">
+			  <span class="field__counter">{{ remainingChars }}</span>
+		  </label>
+	  </div>
+  `,
   styleUrls: ['./input-counter.component.css']
 })
-export class InputCounterComponent implements OnInit, OnChanges {
+export class InputCounterComponent implements OnInit, AfterViewInit {
 
   @Input() textValue: string = "";
-  @Input() maxLength: number = 20;
+  @Input() maxLength: number = 50;
   @Input() placeholderText: string = "";
-  //@ViewChild('inpctr', { static: true }) inputField: HTMLInputElement = new HTMLInputElement();
+  @Input() isReadonly: boolean = false;
+  remainingChars: number;
+  @ViewChild('wuiInput', { static: false }) input: HTMLInputElement = new HTMLInputElement();
 
-  constructor() { }
+  constructor(private el: ElementRef) {
+	this.remainingChars = this.maxLength;
+  }
 
   ngOnInit(): void {
   }
-  
-  ngOnChanges(changes: SimpleChanges) : void {
-	
+
+  ngAfterViewInit(): void {
+	this.input.readOnly = this.isReadonly;
   }
-  
+
+  @HostListener('document:input', ['$event'])
   onInputChange(event: Event) {
-	// const input = document.querySelector('input.field__input');
-	// const counter = document.querySelector('span.field__counter');
-	// const maxLength = this.inputField.getAttribute('maxlength');
-
-	// counter.innerText = this.maxLength;
-
-	// input.addEventListener('input', event => {
-		// const valueLength = event.target.value.length;
-		// const leftCharLength = maxLength - valueLength;
-		
-		// if (leftCharLength < 0) return;
-		// counter.innerText = leftCharLength;
-	// });
+  	if ((<HTMLInputElement>event.target).value.length > 0) {
+  		const valueLength = (<HTMLInputElement>event.target).value.length;
+  		const leftCharLength = this.maxLength - valueLength;
+  		if (leftCharLength < 0) return;
+  		this.remainingChars = leftCharLength;
+  	}
   }
 }
